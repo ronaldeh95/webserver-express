381d2ea1cf7b384a0194b3955f34012c21807a9d
Ronald <ronaldeh95@gmail.com>
2020-10-26 22:38:37 -0600

    Primer commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', function(req, res) {
+
+    res.render('home', {
+        nombre: 'Ronald',
+    });
+})
+
+app.get('/about', function(req, res) {
+
+    res.render('about', {});
+})
+
+
+app.listen(3000, () => {
+    console.log('Escuchando peticiones en el puerto 3000')
+});
